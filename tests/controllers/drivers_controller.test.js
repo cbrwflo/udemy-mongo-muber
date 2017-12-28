46c3e6d7bfefc4f39d0b3cae9c3a8703dc52c3c4
Brown <Carl.Brown194@T-Mobile.com>
2017-12-27 22:51:28 -0800

    Add POST /api/driver route, controller, and test

---
+++ tests/controllers/drivers_controller.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../app');
+
+const Driver = mongoose.model('driver');
+
+describe('Drivers controller', () => {
+  it('POST to /api/drivers/ creates a new driver', done => {
+    Driver.count().then(count => {
+      request(app)
+      .post('/api/drivers')
+      .send({ email: 'test@test.com' })
+      .end(() => {
+        Driver.count().then(newCount => {
+          expect(count + 1).toBe(newCount);
+          done();
+        });
+      });
+    });
+  });
+});
