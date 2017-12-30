d31b78d11472df36677e722dfa58e5258d4ba797
Brown <Carl.Brown194@T-Mobile.com>
2017-12-29 16:21:49 -0800

    Add GET /api/drivers route and tests

--- tests/controllers/drivers_controller.test.js
+++ tests/controllers/drivers_controller.test.js
@@ -6,6 +6,29 @@ const app = require('../../app');
 const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
+  it('GET to /api/drivers finds drivers in a location', done => {
+    const seattleDriver = new Driver({ 
+      email: 'seattle@test.com',
+      geometry: { type: 'Point', coordinates: [-122.4759902, 47.6147628] }
+    });
+
+    const miamiDriver = new Driver({ 
+      email: 'miami@test.com',
+      geometry: { type: 'Point', coordinates: [-80.253, 25.791]}
+    });
+
+    Promise.all([ seattleDriver.save(), miamiDriver.save() ])
+      .then(() => {
+        request(app)
+          .get('/api/drivers?lng=-80&lat=25')
+          .end((err, res) => {
+            expect(res.body.length).toBe(1);
+            expect(res.body[0].obj.email).toBe('miami@test.com');
+            done();
+          });
+      });
+  });
+
   it('POST to /api/drivers creates a new driver', done => {
     Driver.count().then(count => {
       request(app)
